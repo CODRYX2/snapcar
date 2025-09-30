712b4ccaa69b3e05de90e9156050877e001f5f9b
Haydar Berk Karakülah <berkrklh@gmail.com>
2025-09-30 16:20:48 +0300

    Updated ErrorBoundary.tsx

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { AlertTriangle } from 'lucide-react-native';
+import { TriangleAlert as AlertTriangle } from 'lucide-react-native';
 
 interface Props {
   children: React.ReactNode;
